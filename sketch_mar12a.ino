97685a947c43be322705a007050401949d277d0d
riannamueller <rqmu224@uky.edu>
2025-03-12 14:43:40 -0400

    summary
    added 2 lines for left motor control

--- sketch_mar12a.ino
+++ sketch_mar12a.ino
@@ -1,6 +1,8 @@
 void setup() {
   // put your setup code here, to run once:
 BAE Lab
+const int PWMB = 10; //speed control pin on motor driver for left motor
+const int BIN1 = 8; //control pin 1 on the motor driver for left motor
 }
 
 void loop() {
